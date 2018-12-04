7e53f2bfe86bbbd91cab572a5c6a3af729a61c87
Sudip <sudip@sudips-macbook-pro.local>
2018-12-04 18:19:02 +0530

    Initial Commit

---
+++ Wizely ios Task 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wizely ios Task 1
+//
+//  Created by Sudip on 12/4/18.
+//  Copyright © 2018 Sudeepasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
